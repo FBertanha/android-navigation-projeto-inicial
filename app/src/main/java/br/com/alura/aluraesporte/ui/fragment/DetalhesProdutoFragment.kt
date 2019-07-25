86c180b02a2797a00a0fd8b9fee41ba30dfebf43
Felipe Bertanha <bertanhahp@gmail.com>
2019-07-24 22:37:19 -0300

    aula 4,5
    safe args

--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/DetalhesProdutoFragment.kt
@@ -7,9 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import br.com.alura.aluraesporte.R
 import br.com.alura.aluraesporte.extensions.formatParaMoedaBrasileira
-import br.com.alura.aluraesporte.ui.activity.CHAVE_PRODUTO_ID
 import br.com.alura.aluraesporte.ui.viewmodel.DetalhesProdutoViewModel
 import kotlinx.android.synthetic.main.detalhes_produto.*
 import org.koin.android.viewmodel.ext.android.viewModel
@@ -17,9 +17,9 @@ import org.koin.core.parameter.parametersOf
 
 class DetalhesProdutoFragment : Fragment() {
 
+    private val arguments by navArgs<DetalhesProdutoFragmentArgs>()
     private val produtoId by lazy {
-        arguments?.getLong(CHAVE_PRODUTO_ID)
-            ?: throw IllegalArgumentException(ID_PRODUTO_INVALIDO)
+        arguments.produtoId
     }
     private val viewModel: DetalhesProdutoViewModel by viewModel { parametersOf(produtoId) }
     private val navController by lazy { findNavController() }
@@ -51,9 +51,9 @@ class DetalhesProdutoFragment : Fragment() {
     }
 
     private fun vaiParaPagamento() {
-        val dados = Bundle()
-        dados.putLong(CHAVE_PRODUTO_ID, produtoId)
-        navController.navigate(R.id.action_detalhesProduto_to_pagamento, dados)
+        val directions = DetalhesProdutoFragmentDirections
+            .actionDetalhesProdutoToPagamento(produtoId)
+        navController.navigate(directions)
     }
 
     private fun buscaProduto() {
--- app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaProdutosFragment.kt
+++ app/src/main/java/br/com/alura/aluraesporte/ui/fragment/ListaProdutosFragment.kt
@@ -10,7 +10,6 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import br.com.alura.aluraesporte.R
-import br.com.alura.aluraesporte.ui.activity.CHAVE_PRODUTO_ID
 import br.com.alura.aluraesporte.ui.recyclerview.adapter.ProdutosAdapter
 import br.com.alura.aluraesporte.ui.viewmodel.ProdutosViewModel
 import kotlinx.android.synthetic.main.lista_produtos.*
@@ -63,9 +62,9 @@ class ListaProdutosFragment : Fragment() {
     }
 
     private fun vaiParaDetalhesProduto(produtoId: Long) {
-        val bundle = Bundle()
-        bundle.putLong(CHAVE_PRODUTO_ID, produtoId)
-        navController.navigate(R.id.action_listaProdutos_to_detalhesProduto, bundle)
+        val navDirections = ListaProdutosFragmentDirections
+            .actionListaProdutosToDetalhesProduto(produtoId)
+        navController.navigate(navDirections)
     }
 
 }
